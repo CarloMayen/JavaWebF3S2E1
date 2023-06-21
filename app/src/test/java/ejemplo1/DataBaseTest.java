5aa150bd6ca1ac246c6830038b9dea8269ff9c60
Carlo Mayén <carlom.116.cm@gmail.com>
2023-06-20 20:22:16 -0600

    First

---
+++ app/src/test/java/ejemplo1/DataBaseTest.java
@@ -0,0 +1,53 @@
+package ejemplo1;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class DataBaseTest {
+    private DataBase db;
+
+    @BeforeEach
+    void setUp() {
+        db = new DataBase();
+    }
+
+    @Test
+    @DisplayName("The size of an empty database is zero")
+    void testSize() {
+        assertEquals(0, db.size());
+    }
+
+    @Test
+    @DisplayName("DataBase can write data.")
+    void testInsert() {
+        db.insert(new Product(1, "Product 1"));
+        db.insert(new Product(2, "Product 2"));
+        assertEquals(2, db.size());
+    }
+
+    @Test
+    void testGetById() {
+        db.insert(new Product(1, "Product 1"));
+        db.insert(new Product(2, "Product 2"));
+        db.insert(new Product(3, "Product 3"));
+        db.insert(new Product(4, "Product 4"));
+        Product p = new Product(2, "Product 2");
+
+        assertEquals(2, db.getById(2).getId());
+        assertEquals("Product 2", db.getById(2).getName());
+    }
+
+    @Test
+    void testClear() {
+        db.insert(new Product(1, "Product 1"));
+        db.insert(new Product(2, "Product 2"));
+        db.insert(new Product(3, "Product 3"));
+        db.insert(new Product(4, "Product 4"));
+        assertEquals(4, db.size());
+        db.clear();
+        assertEquals(0, db.size());
+    }
+}
